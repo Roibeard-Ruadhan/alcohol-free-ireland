35408bcffa01d2619783e0a41917812d13c59bd5
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-01-04 14:26:56 +0000

    Add new templates with skeleton content. Add content to views.py for post pagination

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -140,7 +140,6 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CSRF_TRUSTED_ORIGINS = ['']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

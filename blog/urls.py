d21cba4a2979425a4e97bffb51fff396c31bbff7
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-01-30 13:49:13 +0000

    Linked up home page to index.html editing urls.py, html & views.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='index'),
+    path('', views.Homepage.as_view(), name="home"),
+    path('blog/', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
+class Homepage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "blog.html"
     paginate_by = 6
 
 

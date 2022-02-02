26e0f43e336eace41e57d5b22103204e83dcbb8f
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-02 12:59:41 +0000

    Add newly created CommentForm() function to views.py on line 37 under the PostDetail function

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )

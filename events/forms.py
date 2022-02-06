2b7c9fc6aeaa096f98ebb61932e4e765577fd425
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-06 14:17:04 +0000

    Add from to add_event.html & set up a new html file to display events=events.html

--- events/forms.py
+++ events/forms.py
@@ -3,20 +3,21 @@ from django.forms import ModelForm
 from .models import Event
 
 class EventForm(ModelForm):
-    model = Event
-    fields = ('name', 'location', 'event_date', 'description' 'attendees_number')
-    labels = {
-        'location': '',
-        'venue': '',
-        'event_date': '',
-        'description': '',
-        'attendees_number': '',
-    }
-    widgets = {
-        'location': froms.TextInput(attrs={''})
-        'venue': froms.TextInput(attrs={''})
-        'event_date': froms.Date Input(attrs={''})
-        'description': froms.TextInput(attrs={''})
-        'attendees_number': froms.NumberInput(attrs={''})
-    }
+    class Meta:
+        model = Event
+        fields = ('name', 'location', 'event_date', 'description' 'attendees_number')
+        labels = {
+            'location': '',
+            'venue': '',
+            'event_date': '',
+            'description': '',
+            'attendees_number': '',
+        }
+        widgets = {
+            'location': froms.TextInput(attrs={'class':'form-control', 'placeholder':'Location'})
+            'venue': froms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue'})
+            'event_date': froms.Date Input(attrs={'class':'form-control', 'placeholder':'Event Date'})
+            'description': froms.TextInput(attrs={'class':'form-control', 'placeholder':'Description'})
+            'attendees_number': froms.NumberInput(attrs={'class':'form-control', 'placeholder':'Maximum number that can attend'})
+        }
 
--- events/models.py
+++ events/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=45)
     Venue = models.Charfield('Event Venue')
     event_date = models.DateTimeField('Event Time')
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, max_length=200)
     attendees_number = models.ManyToManyField('Users Attending')
     # user_name = models.ManyToManyField(?)
 

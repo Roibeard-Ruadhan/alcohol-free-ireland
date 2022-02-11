3a5491f39a02c870668a8777336f2fd0071822a4
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-11 17:50:37 +0000

    Return database to 'DATABASE_URL' from db.sqlit3 after testing

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -30,12 +30,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['non-alc-ireland.herokuapp.com', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-scarlet-silkworm-3mxkukz9.ws-eu25.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-scarlet-silkworm-3mxkukz9.ws-eu25.gitpod.io', 'non-alc-ireland.herokuapp.com']
 
 # Application definition
 

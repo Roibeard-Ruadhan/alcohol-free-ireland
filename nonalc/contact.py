47776cdd01d7a7d05efbb770a2484f61e419964c
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-16 15:48:56 +0000

    Define contact form inside nonalc.contact

--- nonalc/contact.py
+++ nonalc/contact.py
@@ -5,3 +5,21 @@ class ContactForm(forms.Form):
     email = forms.EmailField(required=False, label='Your Email Address')
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
+
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # assert False
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+ 
+    return render(request, 
+        'contact/contact.html', 
+        {'form': form, 'submitted': submitted}
+        )

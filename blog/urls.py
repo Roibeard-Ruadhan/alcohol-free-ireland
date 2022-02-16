88d1cad92ed564e881826fa2e7a9d803d050ba57
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-16 16:42:20 +0000

    Add new fav-icon to blog NavBar

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from . import views
 from django.urls import path, include
+from . import contact
 
 
 urlpatterns = [
     path('', views.Homepage, name="home"),
     path('blog/', views.PostList.as_view(), name='blog'),
     path('events/', include('events.urls'), name='events-urls'),
-    path('contact/', views.contact, name='contact'),
+    path('blog/contact/', views.contact, name='contact'),
     path('add_blog/', views.create_post, name='add_blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),

85022b4f4b81100ac4580853e8769155eeae5a1a
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-17 17:39:51 +0000

    Update add-events url to add-event

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('events/', views.EventList.as_view(), name='events'),
-    path('add_events/', views.add_event, name='add-events'),
+    path('add_event/', views.add_event, name='add-event'),
 ]

0a2a8299e95bdc3e4642553c2baa2f7a8f0abb45
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-02-18 10:22:40 +0000

    Remove Attend Event from the Events dropdown menu inside NavBar. New plan in place to use user image icon instead of a heart icon to simply click to confirm attentence

--- events/admin.py
+++ events/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import events
 # Register your models here.
 admin.site.register(events)
+
+    # def approve_events(self, request, queryset):
+    #     queryset.update(approved=True)

9f5552bc74759019713ba15d6bf36bc18fe9e587
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-06-15 10:45:49 +0000

    Update contact .models & fix button bug for Navbar on smaller screens

--- blog/contact.py
+++ blog/contact.py
@@ -2,26 +2,27 @@ from django import forms
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
+
 class ContactForm(forms.Form):
-    yourname = forms.CharField(max_length=100, label='Your Name')
+    full_name = forms.CharField(max_length=100, label='Your Name')
     email = forms.EmailField(required=False, label='Your Email Address')
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
-def contact(request):
-    submitted = False
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
+# def contact(request):
+#     submitted = False
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
             
-            # assert False
-            return redirect("home")
-    else:
-        form = ContactForm()
-        if 'submitted' in request.GET:
-            submitted = True
+#             # assert False
+#             return redirect("home")
+#     else:
+#         form = ContactForm()
+#         if 'submitted' in request.GET:
+#             submitted = True
  
-    return render(request, 
-        'contact/contact.html', 
-        {'form': form, 'submitted': submitted}
-        )
+#     return render(request, 
+#         'contact/contact.html', 
+#         {'form': form, 'submitted': submitted}
+#         )
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -54,3 +55,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
+class Contact_message(models.Model):
+    """
+    A model to create a contact message which includes
+    the user, their email, the message
+    & the date it's sent
+    """
+
+    class Meta:
+        verbose_name_plural = 'Contact Messages'
+
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=80, null=False, blank=False)
+    subject = models.CharField(max_length=254, null=False, blank=False)
+    message = models.TextField(blank=False, null=False)
+    date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.subject

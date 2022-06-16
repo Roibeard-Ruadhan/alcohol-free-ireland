07f630939ad88e0fbc1a91ce3c4fa2d1d690b9a3
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-06-16 17:21:26 +0000

    Add edit function to events in views & update urls & templates. Test to confirm working

--- events/urls.py
+++ events/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('events/', views.EventList.as_view(), name='events'),
     path('add_event/', views.add_event, name='add-event'),
     path('post_event/<str:location>/', views.PostEvents, name='post_event'),
+    path('edit_event/<int:pk>/', views.edit_event, name="edit_event"),
 ]
 
--- events/views.py
+++ events/views.py
@@ -2,7 +2,7 @@ from .models import events
 from django.views import generic, View
 from blog.models import User
 from datetime import datetime
-from django.http import HttpResponseRedirect,HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from .forms import EventForm
 from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
@@ -19,19 +19,25 @@ class EventList(generic.ListView):
 
 @login_required
 def add_event(request):
+    """ Methon"""
     submitted = False
     form = EventForm()
     if request.method == "POST":
         form = EventForm(request.POST, request.FILES)
+        print("chk post")
         if form.is_valid():
+            print("valid*****************")
             form.save()
-            return redirect('home')
+            return redirect('events')
         else:
+            print("Error")
             form = EventForm
             if 'submitted' in request.POST:
                 submitted = True
 
-    return render(request, 'add_event.html', {'form':form, 'submitted':submitted})
+    return render(request, 'add_event.html', {'form': form, 'submitted':submitted})
+
+
 
 # login required resolves issues with non-users. 
 @login_required  
@@ -48,3 +54,17 @@ def PostEvents(request, location):
     return redirect('events')
 
 
+
+@login_required 
+def edit_event(request, pk):
+    events_obj = events.objects.get(id=pk)
+    form = EventForm(instance=events_obj)
+    if request.method == 'POST':
+        form = EventForm(request.POST, request.FILES, instance=events_obj)
+        if form.is_valid():
+            form.save()
+
+            return redirect('events')
+
+    context = {'form': form}
+    return render(request, 'edit_event.html',context)

bbab0f919d35f7cfe0a39b7b85d50e587c33115a
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-06-19 07:03:09 +0000

    Update README.md & events url

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('/', views.EventList.as_view(), name='events'),
+    path('', views.EventList.as_view(), name='events'),
     path('add_event/', views.add_event, name='add-event'),
     path('post_event/<str:location>/', views.PostEvents, name='post_event'),
     path('edit_event/<int:pk>/', views.edit_event, name="edit_event"),

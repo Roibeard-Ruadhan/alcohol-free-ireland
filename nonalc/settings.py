698769710088f6873ecab894be6df81791520d2f
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-11-04 16:45:47 +0000

    Update settings.py & .env again

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -183,11 +183,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CLOUDINARY = {
-      'cloud_name': 'roibeard',
+      'cloud_name': 'CLOUD-NAME',
       'api_key': '562688784519216',
       'api_secret': 'jvP5_GLgcUzb5qUfxta1g8EScu8',
       'api_proxy': 'http://proxy.server:3128'
 }
 
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
-

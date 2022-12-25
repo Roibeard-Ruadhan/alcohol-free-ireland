935fcd353e1ef55a342ee8d40ba32e62f21bef21
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-12-25 13:22:30 +0000

    Update settings.py

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -30,8 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = False
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 # ALLOWED_HOSTS = ['roibeard.ruadhan.com', 'localhost']
 ALLOWED_HOSTS = ['web-production-01b4.up.railway.app', 'localhost', 'alcohol-free-ireland.onrender.com']

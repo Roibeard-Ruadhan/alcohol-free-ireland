99383d293d09b135aedac64d66eb1235a14b0c67
Roibéard R Coughlan <roibeard.c@outlook.com>
2022-12-27 16:50:50 +0000

    Collect static files & update settings.py

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/' if 'DEVELOPMENT' in os.environ else "https://res.cloudinary.com/roibeard/media/"
+STATIC_URL = '/static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

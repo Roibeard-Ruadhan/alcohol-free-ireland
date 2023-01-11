89c8f52e9e54343aec95429d1e68ce8adfb6d593
Roibéard R Coughlan <roibeard.c@outlook.com>
2023-01-11 20:56:52 +0000

    Remove overflow hidden from CSS to resolve trillions of scroll bars appearing

--- nonalc/settings.py
+++ nonalc/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['roibeard.ruadhan.com', 'localhost']
 ALLOWED_HOSTS = ['*', 'localhost', 'alcohol-free-ireland.onrender.com', 'sober.ireland.roibeardruadhan.com']
